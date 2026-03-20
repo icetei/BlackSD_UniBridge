1469557ce9232e1ae49dcd2b82ccf016001a7364
sherry990105 <sherry99015@gmail.com>
2026-03-20 19:15:02 +0900

    260320 19:14 김재민 게시판 목록 완성

--- unibridge/src/main/java/com/unibridge/app/admin/dao/AdMenteeBoardDAO.java
+++ unibridge/src/main/java/com/unibridge/app/admin/dao/AdMenteeBoardDAO.java
@@ -15,12 +15,18 @@ public class AdMenteeBoardDAO {
 	   public AdMenteeBoardDAO() {
 	      sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	   }
-
+	   //총 개수 조회
 		public int getTotal() {
-			System.out.println("게시글 총 개수 조회 - getTotal 메소드 실행");
+			System.out.println("게시글 총 개수 조회 - getTotal");
 			return sqlSession.selectOne("admin.menteeGetTotal");
 		}
 	   
+		//필터링 개수 조회
+		public int getRenderingTotal(Map<String, Integer> pagefilter) {
+			System.out.println("게시글 필터 후 개수 조회 - getRenderingTotal");
+			return sqlSession.selectOne("admin.menteeGetRenderingTotal", pagefilter);
+		}
+		
 		//멘티 게시판 전체 목록 확인
 		public List<AdMenteeBoardListDTO> selectAll(Map<String, Integer> pageRow){
 			System.out.println("모든 게시글 조회하기");
--- unibridge/src/main/java/com/unibridge/app/admin/dao/AdMentorBoardDAO.java
+++ unibridge/src/main/java/com/unibridge/app/admin/dao/AdMentorBoardDAO.java
@@ -15,27 +15,40 @@ public class AdMentorBoardDAO {
 	   public AdMentorBoardDAO() {
 	      sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	   }
-
+	   //총 개수 조회
 		public int getTotal() {
-			System.out.println("게시글 총 개수 조회 - getTotal 메소드 실행");
-			return sqlSession.selectOne("admin.mentorGetTotal");
+			System.out.println("게시글 총 개수 조회 - getTotal");
+			return sqlSession.selectOne("admin.menteeGetTotal");
 		}
 	   
-		//멘토 게시판 전체 목록 확인
+		//필터링 개수 조회
+		public int getRenderingTotal(Map<String, Integer> pagefilter) {
+			System.out.println("게시글 필터 후 개수 조회 - getRenderingTotal");
+			return sqlSession.selectOne("admin.menteeGetRenderingTotal", pagefilter);
+		}
+		
+		//멘티 게시판 전체 목록 확인
 		public List<AdMentorBoardListDTO> selectAll(Map<String, Integer> pageRow){
 			System.out.println("모든 게시글 조회하기");
-			List<AdMentorBoardListDTO> list = sqlSession.selectList("admin.mentorSelectAll", pageRow);
+			List<AdMentorBoardListDTO> list = sqlSession.selectList("admin.menteeSelectAll", pageRow);
 			System.out.println("조회 결과 " + list);
 			return list;
 		}
 		
 		
-	   //멘토 게시판 필터링 목록 확인
+	   //멘티 게시판 필터링 목록 확인
 		public  List<AdMentorBoardListDTO> selectFilter(Map<String, Integer> pagefilter){
 			System.out.println("랜더링 게시글 조회하기 - selectFilter 메소드 실행 : " + pagefilter);
-			List<AdMentorBoardListDTO> list = sqlSession.selectList("admin.mentorSelectFilter", pagefilter);
+			List<AdMentorBoardListDTO> list = sqlSession.selectList("admin.menteeSelectFilter", pagefilter);
 			System.out.println("조회 결과 : " + list);
 			return list;
+		}
+		
+		//멘티 게시판 조회수
+		public void menteeUpdateReadCount(int boardNumber) {
+			System.out.println("조회수 업데이트 실행");
+			int result = sqlSession.update("admin.menteeUpdateReadCount", boardNumber);
+			System.out.println("조회수 업데이트 완료" + result);
 		}
 		
 		
